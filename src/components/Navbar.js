d06ac504baabe65f877506fe957bbb8dd3ee6226
David <davidkuron1996@gmail.com>
2025-10-30 09:05:59 +0300

    add this file

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -8,10 +8,10 @@ function Navbar() {
     <nav className="navbar">
       <h1 className="logo">🏆Premier League</h1>
       <ul className="nav-links">
-        <li><Link to="/">Teams</Link></li>
-        <li><Link to="/fixtures">Fixtures</Link></li>
-        <li><Link to="/table">Table</Link></li>
-        <li><Link to="/top-players">Top Players</Link></li>
+        <li className="nav-item"><Link to="/">Teams</Link></li>
+        <li className="nav-item"><Link to="/fixtures">Fixtures</Link></li>
+        <li className="nav-item"><Link to="/table">Table</Link></li>
+        <li className="nav-item"><Link to="/top-players">Top Players</Link></li>
       </ul>
     </nav>
   );
